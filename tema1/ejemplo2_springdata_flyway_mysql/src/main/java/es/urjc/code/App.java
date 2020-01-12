10a7c804cc73da81bc8dd6c73c86990345021012
J. Manuel Colmenar <josemanuel.colmenar@urjc.es>
2020-01-12 07:47:12 +0100

    Update Tema 1 ejemplo 2

--- tema1/ejemplo2_springdata_flyway_mysql/src/main/java/es/urjc/code/App.java
+++ tema1/ejemplo2_springdata_flyway_mysql/src/main/java/es/urjc/code/App.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Ejemplo de columna JSON en MySQL
  *
  * @author J. Manuel Colmenar
  */
