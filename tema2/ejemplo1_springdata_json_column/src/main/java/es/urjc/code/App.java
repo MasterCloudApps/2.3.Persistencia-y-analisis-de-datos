e773c89dec96680517dd3cfac95ae32843b6648f
J. Manuel Colmenar <josemanuel.colmenar@urjc.es>
2021-02-01 17:20:07 +0100

    Tema 2 - Ejemplo 1 - Columna JSON en MySQL

---
+++ tema2/ejemplo1_springdata_json_column/src/main/java/es/urjc/code/App.java
@@ -0,0 +1,18 @@
+package es.urjc.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Ejemplo de columna JSON en MySQL
+ *
+ * @author J. Manuel Colmenar
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
