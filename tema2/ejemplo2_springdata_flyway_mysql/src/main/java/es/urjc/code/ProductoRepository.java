1f0b683e3a92b9b1d8e429441023216604a2b61b
J. Manuel Colmenar <josemanuel.colmenar@urjc.es>
2021-12-27 23:32:14 +0100

    Tema 2 - Ejemplo 2 - Flyway
    Update to Java 17 and SprintBoot 2.6
    
    Review example

--- tema2/ejemplo2_springdata_flyway_mysql/src/main/java/es/urjc/code/ProductoRepository.java
+++ tema2/ejemplo2_springdata_flyway_mysql/src/main/java/es/urjc/code/ProductoRepository.java
@@ -1,9 +1,6 @@
 package es.urjc.code;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 /**
  * Repositorio para productos.
@@ -11,6 +8,4 @@ import java.util.List;
  *  @author J. Manuel Colmenar
  */
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
-
-
 }
