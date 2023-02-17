ece9765025d3d44a735663f14a08087a882306c6
J. Manuel Colmenar <josemanuel.colmenar@urjc.es>
2023-02-17 12:52:22 +0100

    Tema 3 - Ejemplo 1 - Spring Data and Mongo
    Update to SpringBoot 3

--- tema3/ejemplo1_springdata_mongodb/src/main/java/es/urjc/code/DatabaseLoader.java
+++ tema3/ejemplo1_springdata_mongodb/src/main/java/es/urjc/code/DatabaseLoader.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Controller;
 
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
